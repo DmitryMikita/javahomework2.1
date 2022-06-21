87085f6349e8207504de6cb8ec2e116e78125a19
dmitrymikita <wmists@gmail.com>
2022-06-22 00:37:51 +1000

    variable correction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int cost = 10000; //цена билета
-        int priceonemile = 500; //стоимость одной мили в рублях
-        int bonus = cost / priceonemile;
+        int priceOneMile = 500; //стоимость одной мили в рублях
+        int bonus = cost / priceOneMile;
 
         System.out.println("Начислено миль:" + bonus);
 
