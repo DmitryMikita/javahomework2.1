eeeadd5fe945a6f8fe349b87f5606585353622a8
dmitrymikita <wmists@gmail.com>
2022-06-21 22:16:50 +1000

    Renaming a variable

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int cost = 10000; //цена билета
-        int x = 500; //стоимость одной мили в рублях
-        int bonus = cost / x;
+        int priceonemile = 500; //стоимость одной мили в рублях
+        int bonus = cost / priceonemile;
 
         System.out.println("Начислено миль:" + bonus);
 
