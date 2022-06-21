7551b2ea5061e75d7ef629e7a897138db9e927d8
dmitrymikita <wmists@gmail.com>
2022-06-21 22:07:37 +1000

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int cost = 10000; //цена билета
         int x = 500; //стоимость одной мили в рублях
-        int bonus = cost/x;
+        int bonus = cost / x;
 
         System.out.println("Начислено миль:" + bonus);
 
